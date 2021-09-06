22d4c804fb0c677fb04ce49b7652e4179a7bc0ce
tusher787 <hasan.md@genweb2.com>
2021-09-06 18:20:59 +0600

    method added

--- src/main/java/com/learn/sub/addtwonum/AddTwoNumber.java
+++ src/main/java/com/learn/sub/addtwonum/AddTwoNumber.java
@@ -2,6 +2,11 @@ package com.learn.sub.addtwonum;
 
 public class AddTwoNumber {
 
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return new ListNode();
+
+    }
+
     public static void main(String[] args) {
 
     }
