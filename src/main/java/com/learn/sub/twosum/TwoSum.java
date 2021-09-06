5be65f7449a96ecde75b7abb8c4b4aae02c20774
tusher787 <hasan.md@genweb2.com>
2021-09-06 12:58:08 +0600

    two sum done

--- src/main/java/com/learn/sub/twosum/TwoSum.java
+++ src/main/java/com/learn/sub/twosum/TwoSum.java
@@ -1,4 +1,37 @@
 package com.learn.sub.twosum;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 public class TwoSum {
+
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> integerListHashMap = new HashMap<>();
+        int[] result = new int[2];
+        for(int i=0; i< nums.length; i++){
+            int complement = target-nums[i];
+            if(integerListHashMap.get(complement) != null){
+                result[0] = integerListHashMap.get(complement);
+                result[1] = i;
+                return result;
+            }
+            else{
+                integerListHashMap.put(nums[i], i);
+            }
+
+
+        }
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+       int nums[] = {2,7,11,15, 10, 18, 99};
+       int target = 109;
+       TwoSum twoSum = new TwoSum();
+       int[] arr = twoSum.twoSum(nums, target);
+       System.out.println(Arrays.toString(arr));
+    }
 }
