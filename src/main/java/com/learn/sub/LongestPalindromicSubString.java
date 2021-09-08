3e7634e538feb3919f87954395c8fb9b4d2c19bc
tusher787 <hasan.md@genweb2.com>
2021-09-08 12:56:48 +0600

    LongestPalindromicSubString this time

--- src/main/java/com/learn/sub/LongestPalindromicSubString.java
+++ src/main/java/com/learn/sub/LongestPalindromicSubString.java
@@ -2,20 +2,45 @@ package com.learn.sub;
 
 public class LongestPalindromicSubString {
 
-    public String longestPalindrome(String s) {
-        StringBuilder stringBuilder;
-        for(int i = s.length()-1; i > 0; i-- ){
-            stringBuilder = new StringBuilder(s.substring(0, i));
-            if(stringBuilder.toString().equals(stringBuilder.reverse().toString()))
+    private String isPalindrome(int start, int finish, String s){
+        StringBuilder stringBuilder = new StringBuilder(s.substring(start, finish));
+        if(stringBuilder.toString().equals(stringBuilder.reverse().toString()))
                 return stringBuilder.toString();
+        return null;
+    }
+
+    public String longestPalindrome(String s) {
+        for(int i = 0; i < s.length(); i++){
+            int incStep = s.length() -i;
+            for(int j = 0; j < s.length(); j++){
+                int lastIndex = j+incStep;
+                if(lastIndex > s.length()-1)
+                    break;
+                System.out.println("first:" +j +", last: "+ lastIndex);
+                String res = this.isPalindrome(j, lastIndex, s);
+                if(res!=null)
+                    return res;
+
+            }
         }
 
         return String.valueOf(s.charAt(0));
+
+
+//        StringBuilder stringBuilder;
+//        for(int i = s.length()-1; i > 0; i-- ){
+//            stringBuilder = new StringBuilder(s.substring(0, i));
+//            if(stringBuilder.toString().equals(stringBuilder.reverse().toString()))
+//                return stringBuilder.toString();
+//        }
+
+        //return String.valueOf(s.charAt(0));
     }
 
     public static void main(String[] args) {
 //        String s = "babad";
-        String s = "cbbd";
+//        String s = "cbbd";
+        String s = "bb";
 
         LongestPalindromicSubString longestPalindromicSubString = new LongestPalindromicSubString();
 
