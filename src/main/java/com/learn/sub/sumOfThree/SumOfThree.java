cf0e1342a7882c4389272fa8bf50dffc6c534b80
tusher787 <hasan.md@genweb2.com>
2021-09-16 14:42:09 +0600

    sum of three optimal done

--- src/main/java/com/learn/sub/sumOfThree/SumOfThree.java
+++ src/main/java/com/learn/sub/sumOfThree/SumOfThree.java
@@ -10,29 +10,51 @@ public class SumOfThree {
         Arrays.sort(nums);
         List<List<Integer>> lists = new ArrayList<>();
         for (int i=0; i<nums.length-2; i++){
-            if(i > 0 && nums[i] == nums[i-1])
-                continue;
-            for (int j= i+1; j<nums.length-1; j++){
-                if(j > (i+1) && nums[j] == nums[j-1])
-                    continue;
-                for(int k=j+1; k< nums.length; k++){
-                    if( k > (j+1) && nums[k] == nums[k-1])
-                        continue;
-                    if(nums[i]+nums[j]+nums[k] == 0){
-                        List<Integer> integers = new ArrayList<>();
-                        integers.add(nums[i]);
-                        integers.add(nums[j]);
-                        integers.add(nums[k]);
-                        lists.add(integers);
+            if(i == 0 || nums[i] != nums[i - 1]){
+                int low = i+1;
+                int high = nums.length-1;
+                int sum = 0-nums[i];
+                while (low < high){
+                    if(nums[low]+nums[high] == sum)
+                    {
+                        //System.out.println(nums[i]+"; "+nums[low]+"; "+ nums[high]);
+                        lists.add(Arrays.asList(nums[i], nums[low], nums[high])); //found a pair
+
+                        while (low < high && nums[low] == nums[low+1]){
+                            //System.out.println("here:"+nums[low]+"; "+nums[low+1]);
+                            low++;
+
+                        }
+                        while (low < high && nums[high] == nums[high-1]){
+                            System.out.println("there:"+nums[low]+"; "+nums[low+1]);
+                            high--;
+
+                        }
+                        low++;
+                        high--;
+
+                    }else if(nums[low]+nums[high] > sum){
+                        //System.out.println("yay");
+                        high--;
                     }
+                    else {
+                        //System.out.println("noo");
+                        low++;
+                    }
+
                 }
+
+
             }
+
         }
         return lists;
+
     }
 
     public static void main(String[] args) {
 //        int[] nums = {-1,0,1,2,-1,-4};
+        //            -4, -1, -1, 0, 1, 2
         int[] nums = {0,0,0,0};
         SumOfThree sumOfThree = new SumOfThree();
         List<List<Integer>> lists = sumOfThree.threeSum(nums);
