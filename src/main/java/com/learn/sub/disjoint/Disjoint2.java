4544f100ab49ae16be1d290e0a5292f7b483f468
tusher787 <hasan.md@genweb2.com>
2021-09-20 15:56:25 +0600

    Disjoint2 done

--- src/main/java/com/learn/sub/disjoint/Disjoint2.java
+++ src/main/java/com/learn/sub/disjoint/Disjoint2.java
@@ -2,22 +2,25 @@ package com.learn.sub.disjoint;
 
 public class Disjoint2 {
     public int createDisjoint(int[] vals) {
-
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
-
+        boolean isDescending = true;
         for (int i = 0; i < vals.length-1; i++) {
             max = Math.max(vals[i+1], max);
             min = Math.min(vals[i], min);
+            if(vals[i] < vals[i+1])
+                isDescending = false;
         }
 
-        return max-min;
+        return isDescending ? 0 : max-min;
     }
 
     public static void main(String[] args) {
         Disjoint2 disjoint2 = new Disjoint2();
 //        int arr[] ={7,1,5,3,6,4};
-        int arr[] ={7,6,4,3,1};
+//        int arr[] ={1,1,5,3,6,10};
+        int arr[] ={10, 9, 8, 7, 20};
+//        int arr[] ={7,6,4,3,1};
         System.out.println(disjoint2.createDisjoint(arr));
     }
 }
