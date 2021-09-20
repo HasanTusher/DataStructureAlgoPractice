d13e62664596a7d028fc074c8828a4ca5016c4ec
tusher787 <hasan.md@genweb2.com>
2021-09-20 16:26:32 +0600

    segment array created

---
+++ src/main/java/com/learn/sub/segment/SegmentTree.java
@@ -0,0 +1,26 @@
+package com.learn.sub.segment;
+
+import java.util.Arrays;
+
+public class SegmentTree {
+
+
+
+    public void createSegmentTree(int[] arr){
+
+        int segTreeLength = arr.length * 2;
+        int segmentTree[] = new int[segTreeLength];
+        for (int i = arr.length-1; i >= 0; i--) {
+            segmentTree[arr.length+i] = arr[i];
+        }
+        System.out.println(Arrays.toString(segmentTree));
+
+    }
+
+    public static void main(String[] args) {
+        SegmentTree segmentTree = new SegmentTree();
+        int[] arr = {1,2,4,5,7,8};
+        segmentTree.createSegmentTree(arr);
+
+    }
+}
