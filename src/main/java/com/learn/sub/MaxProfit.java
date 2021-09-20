418aa3c00d3a42a8b8b8f931bdfd05a39366016f
tusher787 <hasan.md@genweb2.com>
2021-09-20 17:28:01 +0600

    basic MaxProfit done

--- src/main/java/com/learn/sub/MaxProfit.java
+++ src/main/java/com/learn/sub/MaxProfit.java
@@ -3,9 +3,18 @@ package com.learn.sub;
 public class MaxProfit {
 
     public int maxProfit(int[] vals) {
-
+        int maxProfit = 0;
+        for (int i = 0; i < vals.length-1; i++) {
+            for (int j = i+1; j < vals.length; j++) {
+                maxProfit = Math.max(maxProfit, vals[j] -vals[i]);
+            }
+        }
+        return maxProfit;
     }
     public static void main(String[] args) {
-
+        MaxProfit maxProfit = new MaxProfit();
+//        int[] arr = {2,1,2,0,1};
+        int[] arr = {2,1,0,0,0};
+        System.out.println(maxProfit.maxProfit(arr));;
     }
 }
