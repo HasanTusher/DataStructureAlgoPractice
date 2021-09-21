4361d9c713dbcdd29450f1248d3b289d6bf842c8
tusher787 <hasan.md@genweb2.com>
2021-09-21 14:28:41 +0600

    segment tree max done

--- src/main/java/com/learn/sub/segment/SegmentTree.java
+++ src/main/java/com/learn/sub/segment/SegmentTree.java
@@ -1,5 +1,6 @@
 package com.learn.sub.segment;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class SegmentTree {
@@ -12,10 +13,37 @@ public class SegmentTree {
         this.arr = arr;
         this.n = arr.length;
         this.tree = new int[2*n];
-        this.createSegmentTree(arr);
+        this.createMaxSegmentTree(arr);
     }
 
-    public void createSegmentTree(int[] arr){
+    public int findMax(int start, int end){
+        //add +1 to end
+        start+=this.n;
+        end+=this.n;
+        if(start==end)
+            return this.arr[start];
+        int max = Integer.MIN_VALUE;
+
+        while (start < end){
+            if((start & 1) == 1){ //start is odd
+                max = Math.max(this.tree[start], max);
+                start++;
+            }
+            if((end & 1) == 1){
+                end--;
+                max = Math.max(this.tree[end], max);
+            }
+
+            start >>=1;
+            end >>=1;
+        }
+
+        return max;
+
+
+    }
+
+    public void createMaxSegmentTree(int[] arr){
         System.arraycopy(this.arr, 0, this.tree, n, n);
         for(int i = arr.length-1; i>0; i--){
             this.tree[i] = Math.max(this.tree[2*i], this.tree[2*i+1]);
@@ -26,7 +54,9 @@ public class SegmentTree {
     public static void main(String[] args) {
         int[] arr = {1,2,4,5,7,8};
         SegmentTree segmentTree = new SegmentTree(arr);
-        //segmentTree.createSegmentTree(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(segmentTree.findMax(0, arr.length-i));
+        }
 
     }
 }
