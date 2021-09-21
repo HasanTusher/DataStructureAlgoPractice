3f75355b34a47e0539fcf9877f3f1bd5fd2e7ada
tusher787 <hasan.md@genweb2.com>
2021-09-21 17:56:20 +0600

    sum function created

--- src/main/java/com/learn/sub/fenwick/FenwickTree.java
+++ src/main/java/com/learn/sub/fenwick/FenwickTree.java
@@ -26,9 +26,23 @@ public class FenwickTree {
 
     }
 
+    public int sum(int from, int to){
+        int sum = 0;
+        while (from < to){
+            sum+=this.tree[to];
+            to-=(to & -to);
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         int arr[] = {0, 5, 2, 9, -3, 5, 20, 10, -7, 2, 3, -4, 0, -2, 15, 5};
         FenwickTree fenwickTree =new FenwickTree(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(fenwickTree.sum(0, i));
+            //System.out.println(fenwickTree.sum( i));
+        }
     }
 
 }
