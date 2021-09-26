f2898c29e0e41b6e5f0511d677a808fd5fc3eed4
tusher787 <hasan.md@genweb2.com>
2021-09-26 17:29:53 +0600

    course prerequisite done

--- src/main/java/com/learn/sub/courseSchedule/CourseSchedule2.java
+++ src/main/java/com/learn/sub/courseSchedule/CourseSchedule2.java
@@ -1,11 +1,10 @@
 package com.learn.sub.courseSchedule;
 
-import java.util.HashSet;
-import java.util.Stack;
+import java.util.*;
 
 public class CourseSchedule2 {
 
-    private boolean checkIfCanBeCompleted(int index, int[][] prerequisites){
+    private boolean checkIfCanBeCompleted(int index, HashMap<Integer, HashSet<Integer>> prerequisites){
         HashSet<Integer> completed = new HashSet<>();
         Stack<Integer> stack = new Stack<>();
         boolean canBeCompleted = true;
@@ -15,13 +14,13 @@ public class CourseSchedule2 {
         while (!stack.isEmpty()){
             int current = stack.pop();
             if(!completed.contains(current)){
-                for (int i = 0; i < prerequisites.length; i++) {
-                    if(prerequisites[i][0] == current){
-                        //add the other one to the
-                        if (prerequisites[i][1] == root)
-                            return false;
-                        stack.add(prerequisites[i][1]);
-                    }
+                HashSet<Integer> integers = prerequisites.get(current);
+                Iterator<Integer> integerIterator = integers.iterator();
+                while (integerIterator.hasNext()){
+                    int temp = integerIterator.next();
+                    if(temp == root)
+                        return false;
+                    stack.add(temp);
                 }
                 completed.add(current);
             }
@@ -35,11 +34,21 @@ public class CourseSchedule2 {
     }
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        for (int i = 0; i < numCourses; i++) {
-            boolean canBeCompleted = this.checkIfCanBeCompleted(i, prerequisites);
-            if(!canBeCompleted){
-                return false;
-            }
+
+        HashMap<Integer, HashSet<Integer>> graph = new HashMap<>();
+
+        for(int i=0;i<numCourses; i++){
+            graph.put(i, new HashSet<>());
+        }
+
+        for (int i = 0; i < prerequisites.length; i++) {
+            graph.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        }
+
+        for (Map.Entry mapElement : graph.entrySet()) {
+            Integer key = (Integer)mapElement.getKey();
+            boolean xx = this.checkIfCanBeCompleted(key, graph);
+            if(!xx)return false;
 
         }
 
@@ -49,9 +58,9 @@ public class CourseSchedule2 {
     public static void main(String[] args) {
 //        3
 //                [[1,0],[2,1]]
-        int arr[][] = {{1,0},{2,1}};
+        int arr[][] = {{1,0},{0,1}};
         CourseSchedule2 courseSchedule = new CourseSchedule2();
-        boolean xx = courseSchedule.canFinish(3, arr);
+        boolean xx = courseSchedule.canFinish(2, arr);
         System.out.println(xx);
     }
 }
