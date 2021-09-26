8b4c612e58a3d98c3c06e33601c76b6e836bd299
tusher787 <hasan.md@genweb2.com>
2021-09-26 17:08:47 +0600

    new file created

---
+++ src/main/java/com/learn/sub/courseSchedule/CourseSchedule2.java
@@ -0,0 +1,57 @@
+package com.learn.sub.courseSchedule;
+
+import java.util.HashSet;
+import java.util.Stack;
+
+public class CourseSchedule2 {
+
+    private boolean checkIfCanBeCompleted(int index, int[][] prerequisites){
+        HashSet<Integer> completed = new HashSet<>();
+        Stack<Integer> stack = new Stack<>();
+        boolean canBeCompleted = true;
+
+        stack.add(index);
+        int root = index;
+        while (!stack.isEmpty()){
+            int current = stack.pop();
+            if(!completed.contains(current)){
+                for (int i = 0; i < prerequisites.length; i++) {
+                    if(prerequisites[i][0] == current){
+                        //add the other one to the
+                        if (prerequisites[i][1] == root)
+                            return false;
+                        stack.add(prerequisites[i][1]);
+                    }
+                }
+                completed.add(current);
+            }
+
+
+        }
+
+        return canBeCompleted;
+
+
+    }
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        for (int i = 0; i < numCourses; i++) {
+            boolean canBeCompleted = this.checkIfCanBeCompleted(i, prerequisites);
+            if(!canBeCompleted){
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+//        3
+//                [[1,0],[2,1]]
+        int arr[][] = {{1,0},{2,1}};
+        CourseSchedule2 courseSchedule = new CourseSchedule2();
+        boolean xx = courseSchedule.canFinish(3, arr);
+        System.out.println(xx);
+    }
+}
