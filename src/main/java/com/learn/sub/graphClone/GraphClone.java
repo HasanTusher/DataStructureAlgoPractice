e3cc2325e1389b8ccdc733a5d054f3112bd026c9
tusher787 <hasan.md@genweb2.com>
2021-09-26 14:23:54 +0600

    basic done

--- src/main/java/com/learn/sub/graphClone/GraphClone.java
+++ src/main/java/com/learn/sub/graphClone/GraphClone.java
@@ -4,6 +4,24 @@ import java.util.*;
 
 public class GraphClone {
 
+    private Node createGraphFromArrays2(List<List<Integer>> arr) {
+        if(arr.size() == 0)
+            return new Node(1);
+
+        Node[] listOfNodes = new Node[arr.size()];
+        for (int i = 0; i < arr.size(); i++) {
+            listOfNodes[i] = new Node(i+1);
+        }
+
+        for(int i = 0; i< arr.size(); i++){
+            int temp = i+1; // val of the node
+            for (int j = 0; j < arr.get(i).size(); j++) {
+                listOfNodes[i].neighbors.add(listOfNodes[arr.get(i).get(j)-1]);
+            }
+        }
+        return listOfNodes[0];
+    }
+
     private List<Integer> getTraversalVals(Node current, HashSet<Node> alreadyTraversed, Queue<Node> graph) {
         List<Node> nodes = current.neighbors;
         List<Integer> integers = new ArrayList<>();
@@ -37,27 +55,28 @@ public class GraphClone {
 
         }
 
-        return node;
+        return this.createGraphFromArrays2(lists);
 
     }
 
 
 
     public Node cloneGraph(Node node) {
-
-
         return this.traverseAndClone(node);
     }
     public static void main(String[] args) {
         int arr[][] = {{2,4},{1,3},{2,4},{1,3}};
         GraphClone graphClone  = new GraphClone();
         Node aa = graphClone.createGraphFromArrays(arr);
-        graphClone.cloneGraph(aa);
+        Node x = graphClone.cloneGraph(aa);
+        System.out.println(x);
     }
 
+
+
     private Node createGraphFromArrays(int[][] arr) {
-       if(arr[0].length == 0)
-           return new Node(1);
+        if(arr[0].length == 0)
+            return new Node(1);
 
         Node[] listOfNodes = new Node[arr.length];
         for (int i = 0; i < arr.length; i++) {
