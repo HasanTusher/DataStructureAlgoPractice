6ba4d931389a1e6a2d26a823ac48467a5a30757d
tusher787 <hasan.md@genweb2.com>
2021-09-28 14:48:45 +0600

    LongestSubSequence accepted

--- src/main/java/com/learn/sub/longestSubSequence/LongestSubSequence.java
+++ src/main/java/com/learn/sub/longestSubSequence/LongestSubSequence.java
@@ -1,36 +1,35 @@
 package com.learn.sub.longestSubSequence;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class LongestSubSequence {
 
     public int longestConsecutive(int[] nums) {
-        int res = 0;
+        if(nums.length==0)
+            return 0;
+        if(nums.length==1)
+            return 1;
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length-1; i++) {
-            int start = i;
+        int resArray[] = new int[nums.length];
+        resArray[0] =1;
 
-            for (int j = start; j < nums.length-1; j++) {
-                if(nums[j+1] - nums[j] > 1)
-                {
-                    res = Math.max(res, (j-start)+1);
-                    i=j;
-                    break;
-                };
-
-                if(j == nums.length-2){
-                    res = Math.max(res, (j-start)+1);
-                    i=j;
-                }
-            }
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[i] - nums[i-1] == 1){
+                resArray[i] = resArray[i-1]+1;
+            }else if(nums[i] - nums[i-1] == 0){
+                resArray[i] = resArray[i-1];
+            } else
+                resArray[i] = 1;
         }
-        return res;
+        Arrays.sort(resArray);
+        return resArray[resArray.length-1];
     }
 
     public static void main(String[] args) {
 //        int nums[] = {100,4,200,1,3,2};
-//        int nums[] = {0,3,7,2,5,8,4,6,0,1};
-        int nums[] = {0,1};
+        int nums[] = {0,3,7,2,5,8,4,6,0,1};
+//        int nums[] = {0,1};
         LongestSubSequence longestSubSequence = new LongestSubSequence();
         int x = longestSubSequence.longestConsecutive(nums);
         System.out.println(x);
