5cc63d7dac244381325f2225d71123d4bee7a4d9
tusher787 <hasan.md@genweb2.com>
2021-10-10 18:03:20 +0600

    almost done

--- src/main/java/com/learn/sub/BinaryTreeLevelOrderTraversal.java
+++ src/main/java/com/learn/sub/BinaryTreeLevelOrderTraversal.java
@@ -1,12 +1,36 @@
 package com.learn.sub;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class BinaryTreeLevelOrderTraversal {
-    public List<List<Integer>> levelOrder(TreeNode root) {
 
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if(root == null)
+            return Collections.emptyList();
+        List<List<Integer>> left = this.levelOrder(root.left);
+        List<List<Integer>> right = this.levelOrder(root.right);
+
+        List<List<Integer>> res = new ArrayList<>();
+        if(root.left !=null || root.right != null)
+        {
+            List<Integer> tempRes = new ArrayList<>();
+            if(root.left!=null)
+                tempRes.add(root.left.val);
+            if(root.right!=null)
+                tempRes.add(root.right.val);
+            res.add(tempRes);
+        }
+
+        if(!left.isEmpty())
+            res.addAll(left);
+        if(!right.isEmpty())
+            res.addAll(right);
+
+        return res;
     }
 
 
@@ -15,6 +39,7 @@ public class BinaryTreeLevelOrderTraversal {
         TreeNode treeNode = binaryTreeLevelOrderTraversal.createTree();
         List<List<Integer>> lists = binaryTreeLevelOrderTraversal.levelOrder(treeNode);
         lists.forEach(r-> r.forEach(System.out::println));
+        System.out.println("ayya");
     }
 
     public TreeNode createTree() {
