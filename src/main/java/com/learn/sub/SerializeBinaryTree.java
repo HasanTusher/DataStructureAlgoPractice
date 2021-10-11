1173d965e10cf5803f6b6abf6d5a144c82c08434
tusher787 <hasan.md@genweb2.com>
2021-10-11 15:32:11 +0600

    SerializeBinaryTree file created

---
+++ src/main/java/com/learn/sub/SerializeBinaryTree.java
@@ -0,0 +1,28 @@
+package com.learn.sub;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SerializeBinaryTree {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        return null;
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        // Your Codec object will be instantiated and called as such:
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(10, 20);
+
+        SerializeBinaryTree ser = new SerializeBinaryTree();
+        SerializeBinaryTree deser = new SerializeBinaryTree();
+//     TreeNode ans = deser.deserialize(ser.serialize(root));
+
+    }
+}
