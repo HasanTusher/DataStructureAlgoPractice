72594c5147bf8eda933afafd82924138326aaddc
tusher787 <hasan.md@genweb2.com>
2021-10-11 16:59:52 +0600

    SubTreeOfAnotherTree.java done

--- src/main/java/com/learn/sub/SubTreeOfAnotherTree.java
+++ src/main/java/com/learn/sub/SubTreeOfAnotherTree.java
@@ -2,13 +2,54 @@ package com.learn.sub;
 
 public class SubTreeOfAnotherTree {
 
-    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+//    private boolean isSameTree(TreeNode root, TreeNode subRoot){
+//        if(root==null && subRoot==null)
+//            return true;
+//        if(root == null && subRoot!=null)
+//            return false;
+//        if(subRoot == null)
+//            return false;
+//        if(root.val != subRoot.val)
+//            return false;
+//        else
+//            return isSameTree(root.left, subRoot.left) && isSubtree(root.right, subRoot.right);
+//    }
+boolean areIdentical(TreeNode p, TreeNode q)
+{
+
+    /* base cases */
+    if(p == null && q != null)
+        return false;
+    if(q == null && p != null)
+        return false;
+    if(p == null && q == null)
         return true;
+    if(p.val == q.val)
+        return areIdentical(p.left, q.left) && areIdentical(p.right, q.right);
+    else
+        return false;
+}
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (subRoot == null)
+            return true;
+
+        if (root == null)
+            return false;
+
+        if (this.areIdentical(root, subRoot))
+            return true;
+
+
+        return isSubtree(root.left, subRoot)
+                || isSubtree(root.right, subRoot);
     }
     public static void main(String[] args) {
         SubTreeOfAnotherTree subTreeOfAnotherTree = new SubTreeOfAnotherTree();
         TreeNode originalTree = subTreeOfAnotherTree.createTree();
         TreeNode subTree = subTreeOfAnotherTree.createTree2();
+        boolean x = subTreeOfAnotherTree.isSubtree(originalTree, subTree);
+        System.out.println(x);
     }
 
     private TreeNode createTree() {
