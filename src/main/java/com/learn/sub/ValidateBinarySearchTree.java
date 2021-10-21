bfc85516ef1c8867b3575ed57ab149109da80068
tusher787 <hasan.md@genweb2.com>
2021-10-21 16:00:23 +0600

    ValidateBinarySearchTree.java done

--- src/main/java/com/learn/sub/ValidateBinarySearchTree.java
+++ src/main/java/com/learn/sub/ValidateBinarySearchTree.java
@@ -1,43 +1,46 @@
 package com.learn.sub;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ValidateBinarySearchTree {
+    List<Integer> integers;
 
     public boolean isValidBST(TreeNode root) {
-        if(root == null)
-            return true;
-        if(root.right == null && root.left == null)
-            return true;
-
-        boolean isLeftSubTreeValid = isValidBST(root.left);
-        boolean isRightSubTreeValid = isValidBST(root.right);
-        if(isLeftSubTreeValid && isRightSubTreeValid && this.isRootValid(root))
-        {
-            return true;
-        }else
-            return false;
-    }
-
-    private boolean isRootValid(TreeNode root) {
-        if(root.left !=null && root.left.val >= root.val)
-            return false;
-        if(root.right !=null && root.right.val <= root.val)
-            return false;
+        //create the integers
+        this.integers = new ArrayList<>();
+        this.preOrderList(root);
+        for (int i = 0; i < integers.size()-1; i++) {
+            if(this.integers.get(i) >= this.integers.get(i+1))
+                return false;
+        }
         return true;
     }
+
+    private void preOrderList(TreeNode root){
+        if(root == null)
+            return;
+        this.preOrderList(root.left);
+        integers.add(root.val);
+        this.preOrderList(root.right);
+    }
+
 
     public static void main(String[] args) {
         ValidateBinarySearchTree validateBinarySearchTree = new ValidateBinarySearchTree();
         TreeNode treeNode = validateBinarySearchTree.createTree();
-        validateBinarySearchTree.isValidBST(treeNode);
+        Boolean aBoolean = validateBinarySearchTree.isValidBST(treeNode);
     }
 
     private TreeNode createTree() {
         TreeNode head = new TreeNode();
         head = new TreeNode(5, null, null);
-        head.left = new TreeNode(4, null, null);
-        head.right = new TreeNode(6, null, null);
+        head.left = new TreeNode(1, null, null);
+        head.right = new TreeNode(4, null, null);
         head.right.left = new TreeNode(3, null, null);
-        head.right.right = new TreeNode(7, null, null);
+        head.right.right = new TreeNode(6, null, null);
         return head;
     }
 }
